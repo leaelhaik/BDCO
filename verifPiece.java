a639b8c406fbb6c59e26791fc894ffbb4976928e
Lukirt <thomas.duchenne@phelma.grenoble-inp.fr>
2018-04-10 14:39:40 +0200

    Ajout de la verification de la table Piece - A tester a l'imag

--- verifPiece.java
+++ verifPiece.java
@@ -1,12 +1,12 @@
 import java.sql.*;
 
-public class verifTour {
+public class verifPiece {
 
   static final String CONN_URL = "jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensioracle1";
   static final String USER = "dhouibd"; // A remplacer pour votre compte
   static final String PASSWD = "dhouibd";
 
-  public verifTour() {
+  public verifPiece() {
 
     try {
       // Enregistrement du driver Oracle
@@ -18,17 +18,28 @@ public class verifTour {
       Connection conn = DriverManager.getConnection(CONN_URL, USER, PASSWD);
       System.out.println("connected");
       // Creation de la requete
-      PrepareStatement stmt = conn.createStatement("select * from Tour;");
+      PrepareStatement stmt = conn.createStatement("select * from Piece;");
       // Execution de la requete
       ResultSet rset = stmt.executeQuery(STMT);
 
       // Parcours de la TABLE
       System.out.println("*************************");
-      System.out.println("Données contenues dans la table Tour : ");
+      System.out.println("Données contenues dans la table Piece : ");
 
       while(rset.next()) {
-        String nomTour = rset.getString(1);
-        System.out.println("| nomTour : " + nomTour + " |");
+        int idPiece = rset.getInt(1);
+        String typePiece = rset.getString(2);
+        int posX = rset.getInt(3);
+        int posY = rset.getInt(4);
+        int oldX = rset.getInt(5);
+        int oldY = rset.getInt(6);
+        String couleur = rset.getString(7);
+        int numRencontre = rset.getInt(8);
+        String nomTour = rset.getString(9);
+        System.out.println("| idPiece : " + idPiece + " | typePiece : " + typePiece
+        + " | posX : " + posX + " | posY : " + posY + " | oldX : " + oldX + " | oldY : " +
+        oldY + " | couleur : " + couleur + " | numRencontre : " + numRencontre +
+        " | nomTour : " + nomTour + " |");
       }
 
       // Fermeture
@@ -42,6 +53,6 @@ public class verifTour {
   }
 
   public static void main(String args[]) {
-    new verifTour();
+    new verifPiece();
   }
 }
