672b97654fb5c094891b6f83d35cc0d13a99a1c8
Valentin PICARD <valentin.picard@ensimag.grenoble-inp.fr>
2018-04-23 16:41:22 +0200

    Echequier propre

--- src/donnees/Simulateur.java
+++ src/donnees/Simulateur.java
@@ -1,6 +1,7 @@
 package donnees;
 
 import gui.GUISimulator;
+import gui.Text;
 import gui.Rectangle;
 import gui.GraphicalElement;
 import gui.ImageElement;
@@ -53,6 +54,7 @@ public class Simulateur implements Simulable{
 			gui.reset();// clear the window
 			int taille = this.tailleCases;
 			dessineEchiquier(taille);
+			dessineLegende(taille);
 		}
 		
 		/**
@@ -70,14 +72,39 @@ public class Simulateur implements Simulable{
 					x =j*taille;
 					y= i*taille;
 					if ((i%2==0 && j%2!=0 )|| i%2!=0 && j%2==0) {
-						couleur = "#FEFEE0";
-					} else {
 						couleur = "#88421D";
+					} else {
+						couleur = "#FEFEE0";
 					}
 					gui.addGraphicalElement( new Rectangle(x, y, Color.decode(couleur),Color.decode(couleur), taille));
 				}
 			}
 		} //dessiner l'echiquier de base de moi meme
+		
+		/**
+		 * dessine la légende des cases de taille taille
+		 * @param taille la taille des cases
+		 */
+		private void dessineLegende(int taille){
+			java.awt.image.ImageObserver obs=null;		
+			int x = 0;
+			
+			int y = 9;
+			char legende = 65; // Code ASCII du A
+			for (int j = 1; j<= 8; j++) {
+				x = j*taille;
+				gui.addGraphicalElement( new Text(x, y, Color.WHITE, Character.toString(legende)));
+				legende ++;
+			}
+		
+			x = 9;
+			legende = 56; // Code ASCII du 0
+			for (int i = 1; i<=8; i++) {
+				y = i*taille;
+				gui.addGraphicalElement( new Text(x, y, Color.WHITE, Character.toString(legende)));
+				legende --;
+			}
+		}
 		
 	/*	private void dessinePion(int taille) {
 			int i,j=1;
