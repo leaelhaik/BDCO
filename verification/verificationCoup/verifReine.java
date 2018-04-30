89def95bbd1026e6de8647bdd9f0350f121bab99
Donia Dhouib <donia.dhouib@grenoble-inp.org>
2018-04-30 17:00:20 +0200

    verif

--- verification/verificationCoup/verifReine.java
+++ verification/verificationCoup/verifReine.java
@@ -2,9 +2,14 @@ import java.sql.*;
 
 public class VerifReine(){
   public VerifReine(int posY, int oldY, Character posX, Character oldX){
-    if((Math.abs(oldX-posX)<2 && Math.abs(oldY-posY)<2)!=(posX==oldX)||(posY==oldY)){
+    if((Math.abs(oldX-posX)==Math.abs(oldY-posY))!=((posX==oldX)||(posY==oldY))){
       isValid = true;
-      static final String STMT = "select idPiece,couleur as coul,numRencontre as nr, nomTour as nt, posX as px,posY as py from piece where numRencontre=nr and nomTour=nt and coul<>couleur and px between oldX and posX and py between oldY and posY ";
+      verifFou();
+      if((posX==oldX)||(posY==oldY))
+        static final String STMT = "select idPiece, posX as px,posY as py from piece where numRencontre={$numRencontre} and nomTour={$nomTour} and couleur<>{$couleur} and px between {$oldX} and {$posX} and py between {$oldY} and {$posY} ";
+      else
+        static final String STMT = "select idPiece, posX as px,posY as py from piece where numRencontre={$numRencontre} and nomTour={$nomTour} and couleur<>{$couleur} and px between {$oldX} and {$posX} and py between {$oldY} and {$posY}";
+        
     }
   }
 }
--- verification/verificationCoup/verifRoi.java
+++ verification/verificationCoup/verifRoi.java
@@ -4,7 +4,7 @@ public class VerifRoi(){
   public VerifRoi(int posY, int oldY, Character posX, Character oldX){
     if(Math.abs(oldX-posX)<2 && Math.abs(oldY-posY)<2){
       isValid = true;
-      static final String STMT = "select idPiece,couleur as coul,numRencontre as nr, nomTour as nt, posX as px,posY as py from piece where numRencontre=nr and nomTour=nt and coul<>couleur and px between oldX and posX and py between oldY and posY ";
+      static final String STMT = "select idPiece, posX as px,posY as py from piece where numRencontre={$numRencontre} and nomTour={$nomTour} and couleur<>{$couleur} and px between {$oldX} and {$posX} and py between {$oldY} and {$posY} ";
     }
   }
 }
