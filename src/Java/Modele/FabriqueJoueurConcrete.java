eeb30ab5c24717c0ded3bdf5c6683c7fe9128164
Donatien BARRE <donatien.barre@grenoble-inp.org>
2018-05-01 17:46:59 +0200

    Fabrique concrète

---
+++ src/Java/Modele/FabriqueJoueurConcrete.java
@@ -0,0 +1,21 @@
+package Java.Modele;
+
+import Java.Controleur.FabriqueJoueur;
+import Java.Controleur.Joueur;
+import java.util.ArrayList;
+
+/**
+ * Created by ensimag on 01/05/18.
+ */
+public class FabriqueJoueurConcrete extends FabriqueJoueur {
+
+    @java.lang.Override
+    public ArrayList<Joueur> lesJoueurs() {
+        return null;
+    }
+
+    @java.lang.Override
+    public void ajouterJoueur() {
+
+    }
+}
