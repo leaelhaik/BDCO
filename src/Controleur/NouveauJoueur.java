17b23af2e1fc5f98b055da9d269f26bfadce2459
Donatien BARRE <donatien.barre@grenoble-inp.org>
2018-05-05 21:02:23 +0200

    Contrôleur de l'ajout d'un joueur.

---
+++ src/Controleur/NouveauJoueur.java
@@ -0,0 +1,38 @@
+package Controleur;
+
+import Java.Modele.Fabrique;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ensimag on 05/05/18.
+ */
+public class NouveauJoueur {
+
+    String nom;
+    String prenom;
+    String adresse;
+    java.sql.Date date;
+
+    public NouveauJoueur(String nom, String prenom, String adresse, String date) {
+        this.nom = nom;
+        this.adresse = adresse;
+        this.prenom = prenom;
+        SimpleDateFormat birth = new SimpleDateFormat(date);
+        try {
+            Date parsed = birth.parse("20110210");
+            java.sql.Date dateSQL = new java.sql.Date(parsed.getTime());
+            this.date = dateSQL;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean envoyerBD() {
+
+        Fabrique.joueurs.ajouterJoueur(nom, prenom, adresse, date);
+        return false;
+    }
+}
