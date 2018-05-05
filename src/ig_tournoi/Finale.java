b4e4a8177451ca25ac71cd9dc845cf08b51894a7
Lea Elhaik <leaelhaik@gmail.com>
2018-05-05 20:22:48 +0200

    modif mineures

--- src/ig_tournoi/Finale.java
+++ src/ig_tournoi/Finale.java
@@ -10,16 +10,16 @@ public class Finale extends JPanel{
 
 	
 	public Finale() {
+
 		JPanel rencontre1 = new JPanel();
 		//Rencontre r1 = new Rencontre(1);
 		rencontre1.setLayout(new BoxLayout(rencontre1, BoxLayout.LINE_AXIS));
-		rencontre1.add(new JLabel("Rencontre1 : " )); //+ r1.getJoueurBlanc() + " vs " + r1.getJoeurNoir() + "\n"));
+		rencontre1.add(new JLabel("Rencontre1 : " )); //+ r1.getJoueur("blanc") + " vs " + r1.getJoueur("noir") + "\n"));
 		//if (r1.getVainqueur()==null) {
 		rencontre1.add(new JLabel(" -- en cours")); 
 		//} else {
 		//	rencontre1.add(new JLabel("Vainqueur : " + r1.getVainqueur));
 		//}
-		
 		
 		JPanel p = new JPanel();
 		p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
