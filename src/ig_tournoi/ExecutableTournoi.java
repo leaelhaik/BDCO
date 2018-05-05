ac7cff9fc1cdd06f4645384eb1980f19c801fa50
Lea Elhaik <leaelhaik@gmail.com>
2018-05-05 17:22:23 +0200

    avancees IG tournoi

--- src/ig_tournoi/ExecutableTournoi.java
+++ src/ig_tournoi/ExecutableTournoi.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class ExecutableTournoi extends JFrame {
@@ -19,9 +20,13 @@ public class ExecutableTournoi extends JFrame {
 	private InscriptionJoueur ins = new InscriptionJoueur();
 	private JButton boutonSuivreTournoi= new JButton("Suivre un tournoi");
 	private JButton boutonCreerTournoi = new JButton("Creer un nouveau tournoi");
+	private JButton boutonRetour = new JButton("Retour");
 	private final static String TOURNOIEXISTANT = "Tournoi deja existant";
 	private final static String NOUVEAUTOURNOI = "Nouveau tournoi";
-	
+	private final static String QUALIFS = "Phase de qualification";
+	private Qualification qualif = new Qualification();
+	private JLabel tournoi = new JLabel("Tournoi n°54165");
+	private JLabel phase = new JLabel("");
 	
 public ExecutableTournoi(String nomFenetre, int hauteurFenetre, int largeurFenetre) {
 		
@@ -38,23 +43,36 @@ public ExecutableTournoi(String nomFenetre, int hauteurFenetre, int largeurFenet
 	    
 	    boutonSuivreTournoi.addActionListener(new BoutonSuivreTournoiListener());
 	    boutonCreerTournoi.addActionListener(new BoutonCreerTournoiListener());
+	    boutonRetour.addActionListener(new BoutonRetourListener());
 	    
 	    JPanel debut = new JPanel();
 	    debut.add(boutonSuivreTournoi);
 	    debut.add(boutonCreerTournoi);
 	    
 	    JPanel creationTournoi = new JPanel();
-	    JButton validerJoueurs = new JButton("Validez les joueurs");
+	    JButton boutonValiderJoueurs = new JButton("Validez les joueurs");
+	    boutonValiderJoueurs.addActionListener(new BoutonValiderJoueursListener());
+	    //Ici, mettre dans le label tournoi le numero du tournoi avec tournoi.setText("Tournoi n°"+getIdTournoi())
+	    //creationTournoi.add(tournoi, BorderLayout.NORTH);
 	    creationTournoi.add(ins,BorderLayout.CENTER);
-	    creationTournoi.add(validerJoueurs, BorderLayout.SOUTH);
+	    creationTournoi.add(boutonValiderJoueurs, BorderLayout.SOUTH);
+	    creationTournoi.add(boutonRetour, BorderLayout.NORTH);
+	    
 	    
 	    JPanel suiviTournoi = new JPanel();
+	    //creationTournoi.add(boutonRetour, BorderLayout.NORTH);
 	    
+	    JPanel tourQualif = new JPanel();
+	    tourQualif.add(qualif);
+	    //Ici, mettre dans le label tournoi le numero du tournoi avec tournoi.setText("Tournoi n°"+getIdTournoi())
+	    tourQualif.add(tournoi, BorderLayout.NORTH);
+	    tourQualif.add(boutonRetour, BorderLayout.NORTH);
 	    
 	    cards.setLayout(cl);
 	    cards.add(debut);
 	    cards.add(creationTournoi,NOUVEAUTOURNOI);
 	    cards.add(suiviTournoi,TOURNOIEXISTANT);
+	    cards.add(tourQualif,QUALIFS);
 
 	    //JPanel tournoi = new JPanel();
 	    this.setContentPane(cards);
@@ -77,6 +95,23 @@ class BoutonCreerTournoiListener implements java.awt.event.ActionListener {
 		
 	}
 }
-	    
+	 
+
+class BoutonValiderJoueursListener implements java.awt.event.ActionListener {
+
+	public void actionPerformed(ActionEvent arg0) {
+		//tester le nombre de joueurs
+		cl.show(cards, QUALIFS);
+		
+	}
+	
+}
+
+class BoutonRetourListener implements java.awt.event.ActionListener {
+	public void actionPerformed(ActionEvent arg0) {
+		cl.previous(cards);
+		
+	}
+}
 
 }
