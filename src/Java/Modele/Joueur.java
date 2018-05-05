bfeddb6e2b67f6e1643e632503c57c77fa31ff9e
Benoit George <benoit.george@grenoble-inp.fr>
2018-05-05 16:41:37 +0200

    Mise à jours de trucs. Ptit commit en Y

--- src/Java/Modele/Joueur.java
+++ src/Java/Modele/Joueur.java
@@ -5,7 +5,12 @@ package Java.Modele;
  */
 public abstract class Joueur {
 
-    String nom;
+    private String nom;
+    private String prenom;
+    private int idUnique; //l'id unique du joueur pour le tour
+    private String dateNaissance;
+    private String adresse;
+
 
     public Joueur(String name) {
         this.nom = name;
@@ -15,7 +20,29 @@ public abstract class Joueur {
         return true;
     }
 
-    public void miseAJour() {
+    public void miseAJour() {}
 
+//-------getteurs et setteurs -----------
+
+    private void setTout(String nom, String prenom, int idUnique, String dateNaissance, String adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.idUnique = idUnique;
+        this.dateNaissance = dateNaissance;
+        this.adresse = adresse;
+    }
+
+    public int getId() {
+        return this.idUnique;
+    }
+
+
+//-------méthodes------------
+    @Override
+    public String toString(){
+        String s = this.nom + " , " + this.prenom + " , " + this.idUnique;
+        return s;
     }
 }
+
+
