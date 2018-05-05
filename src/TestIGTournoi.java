2a0c3336a50cf837ec85e78998eb2c78caf293dd
Donatien BARRE <donatien.barre@grenoble-inp.org>
2018-05-05 21:03:40 +0200

    Initialisation des fabriques

--- src/TestIGTournoi.java
+++ src/TestIGTournoi.java
@@ -1,12 +1,18 @@
 
+import Java.Modele.Fabrique;
+import Java.Modele.FabriqueJoueurConcrete;
+import Java.Modele.FabriqueRencontreConcrete;
 import ig_tournoi.*;
 
 public class TestIGTournoi {
-	
+
+
 	private static ExecutableTournoi exe;
 
 	public static void main(String args[]) {
+		Fabrique.joueurs = new FabriqueJoueurConcrete();
+		Fabrique.rencontres = new FabriqueRencontreConcrete();
 		exe = new ExecutableTournoi("Tournoi", 700, 700);
-		
+
 	}
 }
