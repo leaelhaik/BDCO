c6c3cb21df5d92ab5434b58efe19996e4db1f37b
dhouibd <donia.dhouib@phelma.grenoble-inp.fr>
2018-05-05 20:16:19 +0200

    getjoueur+getvainqueur

--- src/connection/GetJoueur.java
+++ src/connection/GetJoueur.java
@@ -1,12 +1,12 @@
 package connection;
 import java.sql.*;
 
-public class DebutQuart {
+public class GetJoueur {
 static final String CONN_URL = "jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensioracle1";
 static final String USER = "dhouibd"; // A remplacer pour votre compte
 static final String PASSWD = "dhouibd";
-static final String STMT ="Select idJoueur,nomTour,count(numRencontre) from Rencontre group by(idJoueur) LIMIT 0,8 ORDER by count(numRencontre) ASC";
-public DebutQuart() {
+static final String STMT = "select prenomJoueur,nomJoueur from joueur,affectationCouleur where joueur.idJoueur=affectationCouleur.idJoueur, affectationCouleur.nomCouleur=?,affectationCouleur.numRenconre=?,affectationCouleur.nomTour=?";
+public GetJoueur(String nomTour, int numRenconre, String couleur, Connection conn) {
 try {
 // Enregistrement du driver Oracle
   System.out.print("Loading Oracle driver... ");
@@ -16,12 +16,17 @@ try {
   System.out.print("Connecting to the database... ");
   Connection conn = DriverManager.getConnection(CONN_URL, USER, PASSWD);
   System.out.println("connected");
-  // Creation de la requete
   Statement stmt = conn.createStatement();
-  // Execution de la requete
+
+  // Creation de la requete
+  PreparedStatement sel = conn.prepareStatement(STMT);
+  sel.setString(1,couleur);
+  sel.setInt(2,numRenconre);
+  sel.setString(3,nomTour);
+  sel.executeUpdate();
+
   ResultSet rset = stmt.executeQuery(STMT);
   // Affichage du resultat
-  System.out.println("Results:");
   dumpResultSet(rset);
   System.out.println();
   // Fermeture
@@ -31,6 +36,19 @@ try {
 } catch (SQLException e) {
     System.err.println("failed");
     e.printStackTrace(System.err);
+  }
+}
+private void dumpResultSet(ResultSet rset) throws SQLException {
+  ResultSetMetaData rsetmd = rset.getMetaData();
+  int i = rsetmd.getColumnCount();
+  // for (int k=1;k<=i;k++)
+  //   System.out.print(rsetmd.getColumnName(k) + "\t");
+  // System.out.println();
+  while (rset.next()) {
+    for (int j = 1; j <= i; j++) {
+      System.out.print(rset.getString(j) + "\t");
+    }
+    System.out.println();
   }
 }
 }
