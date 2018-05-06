f05666f4d7383ccaea17626c05a79ebf868e7f8d
Donatien BARRE <donatien.barre@grenoble-inp.org>
2018-05-06 12:12:28 +0200

    Les bons paramètres sont utilisés pour accéder à la bdd

--- src/Java/Modele/FabriqueRencontreConcrete.java
+++ src/Java/Modele/FabriqueRencontreConcrete.java
@@ -3,13 +3,17 @@ package Java.Modele;
 import connection.init.InitAffectationCouleur;
 import connection.insert.InsertRencontre;
 
+import java.sql.Connection;
+
 /**
  * Created by ensimag on 05/05/18.
  */
 public class FabriqueRencontreConcrete extends FabriqueRencontre{
 
-    public FabriqueRencontreConcrete() {
+    private int numRencontre;
 
+    public FabriqueRencontreConcrete() {
+        numRencontre = 0;
     }
 
     @Override
@@ -34,7 +38,11 @@ public class FabriqueRencontreConcrete extends FabriqueRencontre{
         numRencontre : id   | id
         nomCouleur : blanc  | noir
          */
-        InsertRencontre _ = new InsertRencontre();
-        InitAffectationCouleur __ = new InitAffectationCouleur();
+        Connect co = new Connect();
+        Connection conn = co.getConnection();
+        InsertRencontre _ = new InsertRencontre(conn, tour, numRencontre);
+        InitAffectationCouleur __ = new InitAffectationCouleur(conn, "blanc", blanc.getId(), tour, numRencontre);
+        InitAffectationCouleur ___ = new InitAffectationCouleur(conn, "noir", noir.getId(), tour, numRencontre);
+        numRencontre ++;
     }
 }
