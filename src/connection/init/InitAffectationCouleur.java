58a29ec5eef9c1b6aef67ad739908422db4e28b3
dhouibd <donia.dhouib@phelma.grenoble-inp.fr>
2018-05-06 11:12:11 +0200

    initAffectation

--- src/connection/init/InitAffectationCouleur.java
+++ src/connection/init/InitAffectationCouleur.java
@@ -1,59 +1,65 @@
 package connection.init;
 import java.sql.*;
-import java.lang.Math.*;
+//import java.lang.Math.*;
 public class InitAffectationCouleur {
 
-  static final String CONN_URL = "jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensioracle1";
-  static final String USER = "dhouibd"; // A remplacer pour votre compte
-  static final String PASSWD = "dhouibd";
-  static final String STMT = "select idJoueur from Joueur";
-  String STMT1 = "insert into AffectationCouleur values(?,'qualifications',?,?)";
+  // static final String CONN_URL = "jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensioracle1";
+  // static final String USER = "dhouibd"; // A remplacer pour votre compte
+  // static final String PASSWD = "dhouibd";
+  static final String STMT = "insert into AffectationCouleur values(?,?,?,?)";
 
 
-  public InitAffectationCouleur()
+  public InitAffectationCouleur(Connection conn,String couleur,int idJoueur,String nomTour,int numRenconre)
   // String nom, String prenom, String adresse, Date date)
   {
     try {
     // Enregistrement du driver Oracle
-      int i=1;
-      System.out.print("Loading Oracle driver... ");
-      DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-      System.out.println("loaded");
-      // Etablissement de la connection
-      System.out.print("Connecting to the database... ");
-      Connection conn = DriverManager.getConnection(CONN_URL, USER, PASSWD);
-      System.out.println("connected");
+      // int i=1;
+      // System.out.print("Loading Oracle driver... ");
+      // DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+      // System.out.println("loaded");
+      // // Etablissement de la connection
+      // System.out.print("Connecting to the database... ");
+      // Connection conn = DriverManager.getConnection(CONN_URL, USER, PASSWD);
+      // System.out.println("connected");
       conn.setAutoCommit(false);
       // Creation de la requete
       Statement stmt = conn.createStatement();
-      ResultSet rset = stmt.executeQuery(STMT);
-      while(rset.next()){
-          int j = 1;
-          ResultSet rset2 = stmt.executeQuery(STMT);
-          rset2.next();
-          while(rset2.next()){
-            String couleur1="noir";
-            String couleur2="blanc";
-            if(Math.random()==1){
-              couleur1 = "blanc";
-              couleur2="noir";
-            }
-
-            PreparedStatement sel = conn.prepareStatement(STMT1);
-            sel.setInt(1,rset.getInt(1));
-            sel.setInt(2,i);
-            sel.setString(3,couleur1);
-            sel.executeUpdate();
-
-            sel.setInt(1,rset2.getInt(j));
-            sel.setInt(2,i);
-            sel.setString(3,couleur2);
-            sel.executeUpdate();
-            j=j+1;
-            i=i+1;
-          }
-          rset2.close();
-      }
+      PreparedStatement sel = conn.prepareStatement(STMT);
+      sel.setInt(1,idJoueur);
+      sel.setString(2,couleur);
+      sel.setString(3,nomTour);
+      sel.setInt(4,numRenconre);
+      sel.executeUpdate();
+      ResultSet rset = sel.executeQuery(STMT);
+      //ResultSet rset = stmt.executeQuery(STMT);
+      // while(rset.next()){
+      //     int j = 1;
+      //     ResultSet rset2 = stmt.executeQuery(STMT);
+      //     rset2.next();
+      //     while(rset2.next()){
+      //       String couleur1="noir";
+      //       String couleur2="blanc";
+      //       if(Math.random()==1){
+      //         couleur1 = "blanc";
+      //         couleur2="noir";
+      //       }
+      //
+      //       PreparedStatement sel = conn.prepareStatement(STMT1);
+      //       sel.setInt(1,rset.getInt(1));
+      //       sel.setInt(2,i);
+      //       sel.setString(3,couleur1);
+      //       sel.executeUpdate();
+      //
+      //       sel.setInt(1,rset2.getInt(j));
+      //       sel.setInt(2,i);
+      //       sel.setString(3,couleur2);
+      //       sel.executeUpdate();
+      //       j=j+1;
+      //       i=i+1;
+      //     }
+      //     rset2.close();
+      // }
       // Execution de la requete
       conn.commit();
       rset.close();
@@ -71,7 +77,7 @@ public class InitAffectationCouleur {
       // rset13.close();
       // rset14.close();
       stmt.close();
-      conn.close();
+      // conn.close();
     } catch (SQLException e) {
         System.err.println("failed");
         e.printStackTrace(System.err);
@@ -79,6 +85,6 @@ public class InitAffectationCouleur {
     }
 
     public static void main(String args[]) {
-      new InitAffectationCouleur();
+    //  new InitAffectationCouleur();
     }
   }
--- src/connection/insert/InsertRencontre.java
+++ src/connection/insert/InsertRencontre.java
@@ -3,74 +3,79 @@ import java.sql.*;
 
 public class InsertRencontre {
 
-  static final String CONN_URL = "jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensioracle1";
-  static final String USER = "dhouibd"; // A remplacer pour votre compte
-  static final String PASSWD = "dhouibd";
-  static final String STMTCount = "Select COUNT(idJoueur) from Joueur";
+  static final String STMT = "Insert into Rencontre(numRencontre, nomTour, idJoueur) Values(?, ?, ?)";
 
-  public InsertRencontre()
+  // static final String CONN_URL = "jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensioracle1";
+  // static final String USER = "dhouibd"; // A remplacer pour votre compte
+  // static final String PASSWD = "dhouibd";
+  //static final String STMTCount = "Select COUNT(idJoueur) from Joueur";
+
+  public InsertRencontre(Connection conn,String nomTour,int numRenconre)
   // String nom, String prenom, String adresse, Date date)
   {
     try {
     // Enregistrement du driver Oracle
-      System.out.print("Loading Oracle driver... ");
-      DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-      System.out.println("loaded");
-      // Etablissement de la connection
-      System.out.print("Connecting to the database... ");
-      Connection conn = DriverManager.getConnection(CONN_URL, USER, PASSWD);
-      System.out.println("connected");
-      conn.setAutoCommit(false);
+      // System.out.print("Loading Oracle driver... ");
+      // DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+      // System.out.println("loaded");
+      // // Etablissement de la connection
+      // System.out.print("Connecting to the database... ");
+      // Connection conn = DriverManager.getConnection(CONN_URL, USER, PASSWD);
+      // System.out.println("connected");
+      //conn.setAutoCommit(false);
       // Creation de la requete
-      Statement stmt = conn.createStatement();
-      ResultSet rset = stmt.executeQuery(STMTCount);
+      //Statement stmt = conn.createStatement();
+      //ResultSet rset = stmt.executeQuery(STMTCount);
       // Execution de la requete
 
-      int nbJoueurs = 0;
+      //int nbJoueurs = 0;
 
-      while(rset.next()) {
-         nbJoueurs = rset.getInt(1);
-      }
+      // while(rset.next()) {
+      //    nbJoueurs = rset.getInt(1);
+      // }
 
       //System.out.println(nbJoueurs);
       //System.out.println(factorielle());
 
-      int i=1;
-      if(nbJoueurs<8) {
-        System.out.println("Impossible de commencer le tournoi, il faut 8 joueurs.");
-      }
-      else {
-        for(i=1; i<=somme(nbJoueurs)-1; i++) {
-          String STMT = "Insert into Rencontre(numRencontre, nomTour, idJoueur) Values(?, 'qualifications', '0')";
-          PreparedStatement statement = conn.prepareStatement(STMT);
-          statement.setInt(1,i);
-          statement.executeUpdate();
-          statement.close();
-          System.out.println(i);
-        }
-      }
+      // int i=1;
+      // if(nbJoueurs<8) {
+      //   System.out.println("Impossible de commencer le tournoi, il faut 8 joueurs.");
+      // }
+      // else {
+      //   for(i=1; i<=somme(nbJoueurs)-1; i++) {
+      PreparedStatement statement = conn.prepareStatement(STMT);
+      statement.setInt(1,numRenconre);
+      statement.setString(2,nomTour);
+      statement.setNull(3);
+      statement.executeUpdate();
+      ResultatSet rset = statement.executeQuery(STMT);
+      statement.close();
+      //System.out.println(i);
+      //   }
+      // }
 
 
 
       conn.commit();
       rset.close();
-      stmt.close();
-      conn.close();
+      statement.close();
+      //stmt.close();
+      //conn.close();
     } catch (SQLException e) {
         System.err.println("failed");
         e.printStackTrace(System.err);
       }
     }
 
-    private int somme(int n) {
-      int S=1;
-      for(int i=1;i<=n;i++){
-        S=S+i;
-      }
-      return S;
-    }
+    // private int somme(int n) {
+    //   int S=1;
+    //   for(int i=1;i<=n;i++){
+    //     S=S+i;
+    //   }
+    //   return S;
+    // }
 
     public static void main(String args[]) {
-      new InsertRencontre();
+      new InsertRencontre(conn,"qualifications",1);
     }
   }
