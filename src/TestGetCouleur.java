fbf607aaf78425ed33fbe971e3e055c6fe927256
Benoit George <benoit.george@grenoble-inp.fr>
2018-05-06 20:58:38 +0200

    aïe aïe aïe getVainqueur il remarche mtn

---
+++ src/TestGetCouleur.java
@@ -0,0 +1,9 @@
+import Controleur.*;
+
+public class TestGetCouleur {
+
+	public static void main(String args[]){
+		GetCouleur guetteurEnBasDesBlocs = new GetCouleur();
+
+	}
+}
---
+++ src/TestGetMostAdvancedTour.java
@@ -0,0 +1,15 @@
+import Java.Modele.*;
+import Controleur.*;
+
+public class TestGetMostAdvancedTour {
+
+	public static void main(String args[]) {
+
+        ControlleurPhase contr = new ControlleurPhase();
+
+        System.out.println(contr.getMostAdvanceTour());
+
+        System
+
+    }
+}
