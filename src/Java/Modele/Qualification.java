2860dce12a1ea28dd8633558db7e68169177a4f6
Donatien BARRE <donatien.barre@grenoble-inp.org>
2018-05-06 11:15:59 +0200

    Ajout de fonctions pour créer les rencontres en qualifications

--- src/Java/Modele/Qualification.java
+++ src/Java/Modele/Qualification.java
@@ -4,4 +4,23 @@ package Java.Modele;
  * Created by ensimag on 05/05/18.
  */
 public class Qualification extends Tour{
+
+    public void genererRencontre() {
+        ListeJoueurs participants = getJoueurs();
+        for (int i =0; i < participants.getNumberJoueurs() - 1; i++) {
+            for (int j = i + 1; j < participants.getNumberJoueurs(); j++) {
+                if (Math.random() > 0.5) {
+                    Fabrique.rencontres.nouvelleRencontre(participants.getJoueur(i), participants.getJoueur(j), "qualification");
+                } else {
+                    Fabrique.rencontres.nouvelleRencontre(participants.getJoueur(j), participants.getJoueur(i), "qualification");
+                }
+            }
+        }
+    }
+
+    public ListeJoueurs getJoueurs() {
+        ListeJoueurs participants = new ListeJoueurs();
+        participants.getAllJoueurs();
+        return participants;
+    }
 }
