ed1b9a56e33c6f7ef0aecee549f333157315e6ec
dhouibd <donia.dhouib@phelma.grenoble-inp.fr>
2018-05-06 02:30:50 +0200

    verifEchec+coup

--- src/connection/verification/verifEchec.java
+++ src/connection/verification/verifEchec.java
@@ -6,6 +6,7 @@ static final String CONN_URL = "jdbc:oracle:thin:@ensioracle1.imag.fr:1521:ensio
 static final String USER = "dhouibd"; // A remplacer pour votre compte
 static final String PASSWD = "dhouibd";
 static final String STMT = "select idPiece,posX,posY,couleur,typePiece from piece where couleur<>?, numRenconre=?,nomTour=?";
+private boolean enEchec = false ;
 
 //si on a les pos du roi
 public VerifEchec(int posY, Character posX, int numRenconre, String nomTour, String couleur) {
@@ -29,13 +30,15 @@ try {
   sel.executeUpdate();
 
   ResultSet rset = stmt.executeQuery(STMT);
-  switch(rset.getString(5)){
-    case 'pion' : VerifPion(posY,rset.getInt(3),posX,rset.getInt(2));
-    case 'reine' : VerifReine(posY,rset.getInt(3),posX,rset.getInt(2));
-    case 'roi' : VerifRoi(posY,rset.getInt(3),posX,rset.getInt(2));
-    case 'cavalier' : VerifCavalier(posY,rset.getInt(3),posX,rset.getInt(2));
-    case 'tour' : VerifTour(posY,rset.getInt(3),posX,rset.getInt(2));
-    case 'fou' : VerifFou(posY,rset.getInt(3),posX,rset.getInt(2));
+  while(rset.next() && !enEchec){
+    switch(rset.getString(5)){
+      case 'pion' : enEchec = VerifPion(posY,rset.getInt(3),posX,rset.getInt(2)).getIsValid();
+      case 'reine' : enEchec = VerifReine(posY,rset.getInt(3),posX,rset.getInt(2)).getIsValid();
+      case 'roi' : enEchec = VerifRoi(posY,rset.getInt(3),posX,rset.getInt(2)).getIsValid();
+      case 'cavalier' : enEchec = VerifCavalier(posY,rset.getInt(3),posX,rset.getInt(2)).getIsValid();
+      case 'tour' : enEchec = VerifTour(posY,rset.getInt(3),posX,rset.getInt(2)).getIsValid();
+      case 'fou' : enEchec = VerifFou(posY,rset.getInt(3),posX,rset.getInt(2)).getIsValid();
+    }
   }
 
   // Fermeture
@@ -47,18 +50,13 @@ try {
     e.printStackTrace(System.err);
   }
 }
-private void dumpResultSet(ResultSet rset) throws SQLException {
-  ResultSetMetaData rsetmd = rset.getMetaData();
-  int i = rsetmd.getColumnCount();
-  for (int k=1;k<=i;k++)
-    System.out.print(rsetmd.getColumnName(k) + "\t");
-  System.out.println();
-  while (rset.next()) {
-    for (int j = 1; j <= i; j++) {
-      System.out.print(rset.getString(j) + "\t");
-    }
-    System.out.println();
-  }
+
+public boolean enEchec(){
+  return enEchec ;
+}
+
+public static void main(String args[]) {
+  new VerifEchec();
 }
 
 }
