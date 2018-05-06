3e800deb5c3eed6b614e1b58a50789ded6f76819
Donatien BARRE <donatien.barre@grenoble-inp.org>
2018-05-06 12:24:23 +0200

    Génération des rencontres des phases finales.
    Les joueurs doivent être rangés dans l'ordre 1->8 de victoire pour les
    quarts, et cet ordre est gardé pour les 2 tours suivant, le premier
    joueur de la liste étant celui ayant gagné le 1er match (1 contre 8,
    ...), etc...

--- src/Java/Modele/DemiFinal.java
+++ src/Java/Modele/DemiFinal.java
@@ -6,11 +6,17 @@ import Java.Modele.Joueur;
  * Created by georgeb on 4/23/18.
  */
 public class DemiFinal extends PhaseFinale{
-    private ListeJoueurs listeJoueurs;
 
     public DemiFinal(ListeJoueurs listeJoueurs){
         super("demi");
         this.listeJoueurs = new ListeJoueurs();
         super.setJoueurs(listeJoueurs);
+    }
+
+    @Override
+    public void genererRencontre() {
+        for (int i = 0; i < 2; i+=2) {
+            Fabrique.rencontres.nouvelleRencontre(listeJoueurs.getJoueur(i), listeJoueurs.getJoueur(i+1), "demiFinale");
+        }
     }
 }
